1bdacaa51c08d6d3b00789eab6bc18130461aa5d
ericjlie <ericjlie@gmail.com>
2022-01-28 17:54:42 -0800

    chore: fixed _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
 
-function MyApp() {
-  return <div>MyApp</div>
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
