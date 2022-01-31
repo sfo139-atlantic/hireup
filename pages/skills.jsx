d2806188af17c7a2bcc73ddbe1e8f43948396021
Jesse Huang <jjhuang417@gmail.com>
2022-01-30 21:19:04 -0800

    Skills page skeleton created.

--- pages/skills.jsx
+++ pages/skills.jsx
@@ -16,9 +16,7 @@ const headerContainer = `
 const exploreAndMyProfileContainer = `
   flex
   justify-between
-  border
-  border-black
-  w-48
+  w-40
 `;
 
 const hireUpText = `
@@ -50,7 +48,43 @@ const userIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" vie
 <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
 </svg>;
 
+const topHalfFormContainer = `
+  flex
+  flex-col
+`;
+
+const topHalfForm = `
+  flex
+  flex-col
+`
+const labelStyling = `
+  ml-2
+`;
+
+const shortInput = `
+  m-2
+  rounded-md
+  border
+  w-44
+`;
+
+const longInput = `
+  rounded-md
+  m-2
+  border
+  w-1/2
+`;
+
+const checkBoxStyling = `
+  m-2
+`;
+
 export default function skills () {
+  const [userData, setData] = useState();
+  const [userName, setName] = useState('');
+  const [projectManagerCheck, setPMCheck] = useState(false);
+  const [engineerCheck, setEngineerCheck] = useState(false);
+  const [designerCheck, setDesignerCheck] = useState(false);
 
   return (
     <div className={overallContainer}>
@@ -69,14 +103,45 @@ export default function skills () {
           </span>
         </div>
       </div>
+      <div className={topHalfFormContainer}>
+        <form className={topHalfForm}>
+          <input className={shortInput} type='text' name='name' value={userName} placeholder='Name' required></input>
+          <input className={longInput} type='text' name='portfolio' value='' placeholder='Portfolio / Work Example' required></input>
+          <label className={labelStyling}></label>
+          <input className={longInput} type='text' name='portfolio' value='' placeholder='Education' required></input>
+        </form>
+      </div>
       <div>
-        <form>
-          <input type='checkbox'></input>
-          <label>Project Manager</label>
-          <input type='checkbox'></input>
-          <label>Engineer</label>
-          <input type='checkbox'></input>
-          <label>Designer</label>
+        <label className={labelStyling}>My Roles</label>
+      </div>
+      <div className='flex flex-row'>
+        <form className='flex'>
+          <div className={checkBoxStyling}>
+            <input type='checkbox' name='ProjectManager'></input>
+            <label className={labelStyling}>Project Manager</label>
+          </div>
+          <div className={checkBoxStyling}>
+            <input type='checkbox' name='Engineer'></input>
+            <label className={labelStyling}>Engineer</label>
+          </div>
+          <div className={checkBoxStyling}>
+            <input type='checkbox' name='Designer'></input>
+            <label className={labelStyling}>Designer</label>
+          </div>
+        </form>
+      </div>
+      <div className='flex flex-col'>
+        <form className='flex flex-col'>
+          <input className={longInput} type='text' name='WorkHistory' value='' placeholder='Work History' required></input>
+          <input className={longInput} type='text' name='Skills' value='' placeholder='Skills' required></input>
+        </form>
+        <form className='flex flex-row'>
+          <div className='flex flex-col'>
+            <input className={shortInput} type='number' name='HourlyRate' placeholder='0' required></input>
+          </div>
+          <div className='flex flex-col'>
+            <input className={shortInput} type='text' name='Location' value='' placeholder='Location' required></input>
+          </div>
         </form>
       </div>
     </div>
