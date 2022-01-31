c8116698c9deb4581ba5209316283769eccf00fa
bgtdinh <bgtdinh@gmail.com>
2022-01-30 21:52:55 -0800

    Added HireUp Logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -32,20 +32,19 @@ const Navbar = () => {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
                   <img
                     className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    src="/HireUpLogo.svg"
                     alt="Workflow"
                   />
                   <img
                     className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    src="/HireUpLogo.svg"
                     alt="Workflow"
                   />
-                </div>
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
-
                   </div>
                 </div>
               </div>
--- components/NavbarSplash.jsx
+++ components/NavbarSplash.jsx
@@ -34,12 +34,12 @@ const Navbar = () => {
                 <div className="flex-shrink-0 flex items-center">
                   <img
                     className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    src="/HireUpLogo.svg"
                     alt="Workflow"
                   />
                   <img
                     className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    src="/HireUpLogo.svg"
                     alt="Workflow"
                   />
                 </div>
