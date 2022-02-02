a668d4680486594e15ce659d5e297eaaef61f10b
bgtdinh <bgtdinh@gmail.com>
2022-02-01 16:32:59 -0800

    Added Slideshow for splash page

--- components/NavbarSplash.jsx
+++ components/NavbarSplash.jsx
@@ -14,7 +14,7 @@ function classNames(...classes) {
 
 const Navbar = () => {
   return (
-    <Disclosure as="nav" className="fixed top-0 left-0 right-0 bg-transparent">
+    <Disclosure as="nav" className="fixed top-0 left-0 right-0 bg-transparent z-50">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -31,7 +31,10 @@ const Navbar = () => {
                 </Disclosure.Button>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
+              <div className="flex-shrink-0 flex items-center">
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
                   <img
                     className="block lg:hidden h-8 w-auto"
                     src="/HireUpLogo.svg"
@@ -42,10 +45,6 @@ const Navbar = () => {
                     src="/HireUpLogo.svg"
                     alt="Workflow"
                   />
-                </div>
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
-
                   </div>
                 </div>
               </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,37 @@
 import Head from 'next/head';
 import NavbarSplash from '../components/NavbarSplash';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Home() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const featuredImages = ['/ben-rosett-WdJkXFQ4VHY-unsplash.jpg', '/thisisengineering-raeng-TXxiFuQLBKQ-unsplash.jpg', '/pexels-fauxels-3184405.jpg'];
+  let count = 0;
+  const slideRef = useRef();
+
+  useEffect( () => {
+      setInterval(() => {
+        handleNextClick();
+
+      }, 5000);
+      // slideRef.current.addEventListener("animationend", removeAnimation);
+  }, []);
+
+  const handleNextClick = () => {
+    count = (count + 1) % featuredImages.length;
+    setCurrentIndex(count);
+    // slideRef.current.classList.add("fade-anim");
+  };
+
+  const removeAnimation = () => {
+    slideRef.current.classList.remove("fade-anim");
+  }
+
+  const handlePreviousClick = () => {
+    count = (currentIndex + featuredImages.length - 1) % featuredImages.length;
+   setCurrentIndex(count);
+  };
+
+
   return (
     <div>
       <Head>
@@ -9,7 +39,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavbarSplash/>
-      <img src="https://images.unsplash.com/photo-1425421669292-0c3da3b8f529?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb"/>
+      <div className="max-w-full">
+        <div ref={slideRef} className="w-full absolute select-none">
+          <img src={featuredImages[currentIndex]} alt="" />
+          <div className="absolute w-full top-1/2 transform -translate-y-1/2 flex justify-between items-start px-3">
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
