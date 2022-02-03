2c7adb4bd3eccc8a4158f77ce1b28a4552871b00
Jantira Vongampai <janvong.official@gmail.com>
2022-02-03 14:03:52 -0800

    commit before pull update from main branch

--- models/profiles.js
+++ models/profiles.js
@@ -8,7 +8,7 @@ module.exports = {
       throw e;
     }
   },
-  post: async ({id, email}) => {
+  post: async ({_id, email}) => {
     try {
       const result = await db.profiles.findOneAndUpdate({_id: id, email}, {$set:{_id: id, email,}}, {upsert: true});
       return true
@@ -16,11 +16,11 @@ module.exports = {
       throw e;
     }
   },
-  put: async ({id, email, info}) => {
+  put: async ({id, info}) => {
     const {firstName, lastName, freelancer, rate, work_history, skills, education, location, portfolio, proposals, profile_pic, timezones} = info;
     try {
       const result = await db.profiles.updateOne({_id: id},{$set:{
-        firstName, lastName, email, freelancer, rate, work_history, skills, education, location, portfolio, proposals, profile_pic, timezones
+        firstName, lastName, freelancer, rate:Number(Number(rate).toFixed(2)), work_history, skills, education, location, portfolio, proposals, profile_pic, timezones
       }})
       return true;
     } catch(e) {
