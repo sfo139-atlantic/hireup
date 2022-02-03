283fa2dd84e5600f17f2b02977ec5c3abbea57a3
Van Hsieh <van.hsieh1@gmail.com>
2022-02-03 14:02:17 -0800

    fix sidebar highlight bug

--- components/ProposalSidebar.jsx
+++ components/ProposalSidebar.jsx
@@ -2,19 +2,20 @@ import React, { useEffect } from "react";
 
 export default function ProposalForm({ allProposals, currProposal, switchProposal }) {
 
-  console.log('prposalsidebar')
   return (
     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col h-screen">
       <div className="text-xl pt-5 pb-1 mb-4 font-bold border-b-2">All Proposals</div>
       {allProposals.map(proposal => {
+        return (
         <div
-          key={proposal._id}
+          key={proposal.id}
           className={proposal._id === currProposal._id ? "text-white border-2 bg-green rounded-lg font-bold" : ""}
           onClick={() => switchProposal(proposal)}>
           <div className="pl-2">
             {proposal.headline}
           </div>
         </div>
+        )
       }
       )}
         <div
