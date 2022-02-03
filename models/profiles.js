a8591ca36466363d3946608faba46828fe4bc443
Van Hsieh <van.hsieh1@gmail.com>
2022-02-03 15:49:52 -0800

    added routes to support proposals and skills

--- models/profiles.js
+++ models/profiles.js
@@ -59,5 +59,79 @@ module.exports = {
     } catch (e) {
       throw e;
     }
-  }
+  },
+  getById: async (id) => {
+    try {
+      const profiles = await db.profiles.find({ _id: id }).toArray()
+      return profiles;
+    } catch (e) {
+      throw e;
+    }
+  },
+  createProposal: async (proposal) => {
+    try {
+      const newProposal = await db.profiles.updateOne(
+        {_id : proposal.userId},
+        { $push: { proposals : {
+          headline: proposal.headline,
+          overview: proposal.overview,
+          timeline: {
+            start: proposal.start,
+            end: proposal.end
+          },
+          location: proposal.location,
+          timezones: proposal.skills,
+          _id: proposal.id
+        }} }
+      )
+
+      return newProposal
+    } catch (e) {
+      throw e
+    }
+
+  },
+  updateProposal: async (proposal) => {
+    try {
+      const newProposal = await db.profiles.findOneAndUpdate(
+        {_id : proposal.userId, "proposals._id": proposal.id},
+        { $set: {
+            "proposals.$.headline": proposal.headline,
+            "proposals.$.overview": proposal.overview,
+            "proposals.$.timeline": {
+              start: proposal.start,
+              end: proposal.end
+            },
+            "proposals.$.location": proposal.location,
+            "proposals.$.timezones": proposal.timezones,
+            "proposals.$.budget": proposal.budget,
+          }
+        }
+      )
+      return newProposal
+    } catch (e) {
+      console.log(e);
+    }
+
+  },
+  updateSkill: async (skill) => {
+    try {
+      const newSkill = await db.profiles.findOneAndUpdate(
+        {_id : skill.userId},
+        {$set: {
+          firstName: skill.firstName,
+          lastName: skill.lastName,
+          rate: skill.rate,
+          work_history: skill.workHistory,
+          skills: skill.skills,
+          education: skill.education,
+          location: skill.location,
+          portfolio: skill.portfolio,
+          timezones: skill.timezones
+        }}
+      )
+    } catch (e) {
+      throw e;
+    }
+  },
 }
